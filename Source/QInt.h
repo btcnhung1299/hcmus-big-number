347b0c3511323cbe545c878da7d69e393e7ea7fe
Tam NB Nguyen <43813341+nnbtam99@users.noreply.github.com>
2019-03-24 21:56:31 +0700

    Update printQInt, binToHex, decToHex

--- Source/QInt.h
+++ Source/QInt.h
@@ -1,6 +1,6 @@
 #ifndef QINT_H_
 #define QINT_H_
-
+#define MAX_NUM_OF_DIGITS 40
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -30,8 +30,13 @@ class QInt
 		void clearBit(int pos);
 		void changeBit(int pos, bool value);
 	// 	bool* unsignedBin(bool* bit);
-
-	
+		string calculatePowerOf2(int n);
+		void multiply(int res[], int& resSize, int x);
+		string multiplyTwoStrings(string str1, string str2);
+		string addTwoStrings(string str1, string str2);
+		void add(int res[], int a[], int &resSize, int aSize);
+		void swapArr(int a[], int b[], int &aSize, int &bSize);
+		
 	public:
 	 	QInt& operator=(const QInt &);
 	 	bool operator>(const QInt &) const;
@@ -52,13 +57,16 @@ class QInt
 
 	public:
 		void scanQInt();
+		void printQInt();
 		bool* decToBin() const;
 		QInt& binToDec(bool *bits);
 		string binToHex(bool *bits);
 		string decToHex();
-
+		int binToDec4bits(string bin);
+		string* binToHex(bool *bits);
+		string* decToHex(QInt x);
 		static bool* addBitArrays(const bool*, const bool*);
 };
 
 #include "QInt.hpp"
-#endif	/* QINT_H_ */
+#endif QINT_H_ 
