46b6ea63e4dc185b12830adfca086fe9e37470cb
Tu <huynhtu9910@gmail.com>
2019-03-28 13:16:04 +0700

    Khởi tạo lại

--- QFloat/Qfloat.h
+++ QFloat/Qfloat.h
@@ -1,4 +1,4 @@
-#ifndef QFLOAT_H_
+﻿#ifndef QFLOAT_H_
 #define QFLOAT_H_
 
 #include <iostream>
@@ -9,17 +9,17 @@ using namespace std;
 class QFloat
 {
 private:
-	short s_exponent;
-	short significand[7];
-
+	short data[8];// Theo thứ tự: 1 bit dấu, 15 bits mũ, 112 bits trị
 private:
-
+	void setBit(int pos);
+	void clearBit(int pos);
+	void changeBit(int pos, bool value);
 
 public:
 	QFloat();
 	QFloat(const QFloat&);
 
-	void scanQFloat();
+	void scanQFloat(string s);
 	QFloat binToDec(bool*);
 
 
--- QFloat/Qfloat.hpp
+++ QFloat/Qfloat.hpp
@@ -2,21 +2,39 @@
 
 QFloat::QFloat()
 {
-	s_exponent = 0;
-	for (int i = 0; i < 7; i++)
-		significand[i] = 0;
+	for (int i = 0; i < 8; i++)
+		data[i] = 0;
 }
 QFloat::QFloat(const QFloat& another)
 {
-	s_exponent = another.s_exponent;
 	for (int i = 0; i < 7; i++)
-		significand[i] = another.significand[i];
+		data[i] = another.data[i];
 }
 
-void QFloat::scanQFloat()
+
+
+void QFloat::setBit(int pos)
+{
+	int index = pos / 32;
+	int k = pos % 32;
+	data[index] = data[index] | (1 << (31 - k));
+}
+
+void QFloat::clearBit(int pos)
+{
+	int index = pos / 32;
+	int k = pos % 32;
+	data[index] = data[index] & ~(1 << (31 - k));
+}
+
+void QFloat::changeBit(int pos, bool value)
+{
+	value ? setBit(pos) : clearBit(pos);
+}
+
+void QFloat::scanQFloat(string s)
 {
 /*
-- Nhập vào chuỗi ký tự string.
 - Đưa string về dạng chuẩn.
 	* Difficult
 	+ Xác định dấu 
@@ -27,14 +45,41 @@ void QFloat::scanQFloat()
 	+ Đưa phần trị vào dãy bits
 - binToDec(bits)
 */
+	// 1. Đưa về dạng chuẩn.
+	// Kiểm tra (+) hay (-)
+	bool is_negative = false;
+	if (s.front() == '-')
+	{
+		is_negative = true;
+		s.erase(0, 1);
+	}
+
+	// 2. Chuyển về dãy bits[128]
+	bool* bits = new bool[128];
+	for (int i = 0; i < 128; i++)
+		bits[i] = 0;
+
+	if (is_negative)
+		bits[0] = 1;
+
+	//
+	//
+
+	// 3. binToDec
+	binToDec(bits);
+	delete[] bits;
 }
 
 QFloat QFloat::binToDec(bool *bits)
 {
 /*
-- Lưu phần dấu
-- Lưu phần mũ
-- Lưu phần trị 
+Chuyển từ hệ nhị phân sang thập phân (dưới dạng số lớn QFloat)
+Công thức chuyển đổi:
+- Bit tại vị trí thứ i được lưu tại ô (i / 32). Ví dụ: Bit thứ 31 lưu ở ô 0, bit thứ 32 lưu ở ô 1.
+- Trong ô đó, bit thứ i được set giá trị tại vị trí thứ (i % 32). Ví dụ: Bit thứ 0 lưu ở ô thứ 0 tại vị trí 0, Bit thứ 32 lưu ở ô thứ 1 tại vị trí 0.
 */
+	for (int i = 0; i < 128; i++)
+		changeBit(i, bits[i]);
 
+	return *this;
 }
