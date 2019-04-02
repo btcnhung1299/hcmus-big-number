1d98785cd456aa97a572523732686bc1bb2277de
btcnhung.1299 <btcnhung.1299@gmail.com>
2019-04-02 23:09:54 +0700

    Fixed operator +, unfinished underflow

--- Source/QFloat/main.cpp
+++ Source/QFloat/main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 	QFloat a, b;
-	string s1 = "-123.75";
+	string s1 = "-7.0";
 	a.scanQFloat(s1);
-	string s2 = "-999999.125";
+	string s2 = "-4.0";
 	b.scanQFloat(s2);
 	QFloat c = a + b;
 
