fa2757565091095e98700b783fb79ef354d10f58
Tam NB Nguyen <43813341+nnbtam99@users.noreply.github.com>
2019-04-06 22:27:51 +0700

    Create Functions.h

---
+++ Source/Console/Functions.h
@@ -0,0 +1,8 @@
+#include "QInt.h"
+#include "QFloat.h"
+vector<string> splitString(string s);
+QInt twoOperand1(QInt operand_1, QInt operand_2, string o);
+bool twoOperand2(QInt operand_1, QInt operand_2, string o);
+QInt twoOperand3(QInt operand, int k, string o);
+int typeOperator(string o);
+QFloat twoOperand1(QFloat operand_1, QFloat operand_2, string o);
